35dffe63529260def3197bc24ed828efa8f41b22
Elise Muellerleile <79237986+eliamue@users.noreply.github.com>
2021-10-11 16:13:56 -0500

    Setup (#2)
    * setup services and hooks
    
    * appjs updated
    
    * import routes fixed

---
+++ src/components/displays/UserProfile.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import useUsers from '../hooks/useUsers';
+import PropTypes from 'prop-types';
+import useArtists from '../hooks/useArtists';
+
+export default function UserProfile({ match }) {
+  const { userObject, loading } = useUsers();
+  const { artists } = useArtists(match.params.access_token);
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      <img alt="user image" src={userObject.image} />
+      <h2>Welcome, {userObject.displayName}!</h2>
+      <a href={userObject.profileURL}>Spotify profile</a>
+
+      <h3>Your Top Artists</h3>
+      <ul>
+        {userObject
+          ? artists.map((artist) => (
+            <li key={artist.id}>
+              <a href={artist.url}>
+                <div>{artist.name}</div>
+              </a>
+              <img src={artist.images[1].url} />
+              <p>{artist.genres}</p>
+            </li>
+          ))
+          : 'unavailable'}
+        ;
+      </ul>
+    </div>
+  );
+}
+
+UserProfile.propTypes = {
+  artists: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      image: PropTypes.string,
+      url: PropTypes.string,
+      id: PropTypes.string,
+      genres: PropTypes.string,
+    })
+  ),
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      access_token: PropTypes.string.isRequired,
+      refresh_token: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
---
+++ src/components/hooks/useArtists.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { fetchTopArtists } from '../services/spotifyAPI';
+
+export default function useArtists(token) {
+  const [artists, setArtists] = useState([]);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    fetchTopArtists(token)
+      .then((artistArray) => setArtists(artistArray))
+      .finally(() => setloading(false));
+  }, []);
+
+  return { artists, loading };
+}
