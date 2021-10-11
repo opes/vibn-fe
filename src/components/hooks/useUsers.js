84a042e96d817d25ebd3f3780279aa655e24142f
Elise Muellerleile <eliamue@live.com>
2021-10-11 15:56:27 -0500

    setup services and hooks

---
+++ src/components/hooks/useUsers.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { fetchCurrentUser } from '../services/userAuth';
+
+export default function useUsers() {
+  const [userObject, setUserObject] = useState(null);
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    fetchCurrentUser()
+      .then((user) => setUserObject(user))
+      .finally(() => setloading(false));
+  }, []);
+
+  return { userObject, loading };
+}
