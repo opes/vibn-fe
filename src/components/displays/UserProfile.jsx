6d13c3432ca081fb6dda7068e04280c1f012db5c
Sofia Tejada <sofianais@icloud.com>
2021-10-12 12:04:47 -0700

    more git issues

--- src/components/displays/UserProfile.jsx
+++ src/components/displays/UserProfile.jsx
@@ -1,60 +1,42 @@
 import React from 'react';
-import useUsers from '../../hooks/useUsers';
+import useUsers from '../hooks/useUsers';
 import PropTypes from 'prop-types';
-import useArtists from '../../hooks/useArtists';
-import styles from '../../styles/profile.css';
-import linebreak from '../../assets/linebreak.png';
-import icon from '../../assets/spotify-icon.png';
-import Header from './Header';
+import useArtists from '../hooks/useArtists';
 
 export default function UserProfile({ match }) {
+  // first we set the tokens to local storage
+  localStorage.setItem('REFRESH_TOKEN', match.params.refresh_token);
+  localStorage.setItem('ACCESS_TOKEN', match.params.access_token);
+
+  // then we update state
   const { userObject, loading } = useUsers();
-  const { artists } = useArtists(match.params.access_token);
+  const { artists } = useArtists(localStorage.getItem('ACCESS_TOKEN'));
 
   if (loading) {
     return <h1>Loading...</h1>;
   }
 
   return (
-    <div className={styles.profile_main}>
-      <Header />
-      <section className={styles.profile}>
-        <img
-          className={styles.profile_pic}
-          alt="user image"
-          src={userObject.image}
-        />
-        <h2 className={styles.profile_name}>
-          Welcome, {userObject.displayName}!
-        </h2>
-        <a href={userObject.profileURL} className={styles.profile_link}>
-          <img className={styles.spotify_icon} src={icon} alt="Go to your Spotify" />
-        </a>
-      </section>
-      
-      <img src={linebreak} alt="linebreak" />
+    <div>
+      <img alt="user image" src={userObject.image} />
+      <h2>Welcome, {userObject.displayName}!</h2>
+      <a href={userObject.profileURL}>Spotify profile</a>
 
-      <h3 className={styles.top_label}>Your Top Artists</h3>
-
-      <section className={styles.artists_container}>
-        <ul className={styles.artists_list}>
-          {userObject
-            ? artists.map((artist) => (
-              <li className={styles.artists_item} key={artist.id}>
-                <p>
-                  <img className={styles.artist_img} src={artist.images[1].url} />
-                </p>
-                <p className={styles.artist_name}>
-                  <a href={artist.external_urls.spotify} alt={artist.name}>
-                    {artist.name}
-                  </a>
-                </p>
-                {/* <p className={styles.genres}>{artist.genres}</p> */}
-              </li>
-            ))
-            : 'unavailable'}
-        </ul>
-      </section>
+      <h3>Your Top Artists</h3>
+      <ul>
+        {userObject
+          ? artists.map((artist) => (
+            <li key={artist.id}>
+              <a href={artist.url}>
+                <div>{artist.name}</div>
+              </a>
+              <img src={artist.images[1].url} />
+              <p>{artist.genres}</p>
+            </li>
+          ))
+          : 'unavailable'}
+        ;
+      </ul>
     </div>
   );
 }
@@ -64,7 +46,7 @@ UserProfile.propTypes = {
     PropTypes.shape({
       name: PropTypes.string,
       image: PropTypes.string,
-      spotify: PropTypes.string,
+      url: PropTypes.string,
       id: PropTypes.string,
       genres: PropTypes.string,
     })
