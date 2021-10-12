302fb876c6e74b46443e1f9e5bf09b927af88a29
Sofia Tejada <sofianais@icloud.com>
2021-10-12 13:27:53 -0700

    fixed?

--- src/components/displays/UserProfile.jsx
+++ src/components/displays/UserProfile.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import useUsers from '../../hooks/useUsers';
 import PropTypes from 'prop-types';
 import useArtists from '../../hooks/useArtists';
+import styles from '../../styles/profile.css';
+import linebreak from '../../assets/linebreak.png';
+import icon from '../../assets/spotify-icon.png';
+import Header from './Header';
 
 export default function UserProfile({ match }) {
   // first we set the tokens to local storage
@@ -17,26 +21,45 @@ export default function UserProfile({ match }) {
   }
 
   return (
-    <div>
-      <img alt="user image" src={userObject.image} />
-      <h2>Welcome, {userObject.displayName}!</h2>
-      <a href={userObject.profileURL}>Spotify profile</a>
+    <div className={styles.profile_main}>
+      <Header />
+      <section className={styles.profile}>
+        <img
+          className={styles.profile_pic}
+          alt="user image"
+          src={userObject.image}
+        />
+        <h2 className={styles.profile_name}>
+        Welcome, {userObject.displayName}!
+        </h2>
+        <a href={userObject.profileURL} className={styles.profile_link}>
+          <img className={styles.spotify_icon} src={icon} alt="Go to your Spotify" />
+        </a>
+      </section>
 
-      <h3>Your Top Artists</h3>
-      <ul>
-        {userObject
-          ? artists.map((artist) => (
-            <li key={artist.id}>
-              <a href={artist.url}>
-                <div>{artist.name}</div>
-              </a>
-              <img src={artist.images[1].url} />
-              <p>{artist.genres}</p>
-            </li>
-          ))
-          : 'unavailable'}
-        ;
-      </ul>
+      <img src={linebreak} alt="linebreak" />
+
+      <h3 className={styles.top_label}>Your Top Artists</h3>
+
+      <section className={styles.artists_container}>
+        <ul className={styles.artists_list}>
+          {userObject
+            ? artists.map((artist) => (
+              <li className={styles.artists_item} key={artist.id}>
+                <p>
+                  <img className={styles.artist_img} src={artist.images[1].url} />
+                </p>
+                <p className={styles.artist_name}>
+                  <a href={artist.external_urls.spotify} alt={artist.name}>
+                    {artist.name}
+                  </a>
+                </p>
+                {/* <p className={styles.genres}>{artist.genres}</p> */}
+              </li>
+            ))
+            : 'unavailable'}
+        </ul>
+      </section>
     </div>
   );
 }
@@ -46,7 +69,7 @@ UserProfile.propTypes = {
     PropTypes.shape({
       name: PropTypes.string,
       image: PropTypes.string,
-      url: PropTypes.string,
+      spotify: PropTypes.string,
       id: PropTypes.string,
       genres: PropTypes.string,
     })
