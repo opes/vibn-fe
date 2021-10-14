11e8fc34a8ef71cf2e4f4ebab1ba35a49088738c
Brianna Bias <brianna.bias@gmail.com>
2021-10-14 09:50:52 -0700

    fixed window.location

--- src/components/displays/CreateConvo.jsx
+++ src/components/displays/CreateConvo.jsx
@@ -29,7 +29,7 @@ export default function CreateConvo({ match }) {
     });
 
     // window.location.href = `http://localhost:7891/user/${localStorage.getItem('CURRENT_USER_ID')}/conversations`;
-    window.location.href = `https://vib.netlify.app/user/${localStorage.getItem('CURRENT_USER_ID')}/conversations`;
+    window.push.href = `https://vib.netlify.app/user/${localStorage.getItem('CURRENT_USER_ID')}/conversations`;
 
   };
 
