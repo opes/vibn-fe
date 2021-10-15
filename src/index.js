aff94f7c99af03563f243ff51b9d41e3d252b43e
Brianna Bias <brianna.bias@gmail.com>
2021-10-14 17:05:36 -0700

    ahhh

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/App';
-import './assets/fonts/LarabiefontRg-Regular.eot;'
-import './assets/fonts/LarabiefontRg-Regular.svg;'
-import './assets/fonts/LarabiefontRg-Regular.ttf;'
-import './assets/fonts/LarabiefontRg-Regular.woff;'
-import './assets/fonts/LarabiefontRg-Regular.woff2;'
-import './assets/fonts/Streamster.eot;'
-import './assets/fonts/Streamster.svg;'
-import './assets/fonts/Streamster.ttf;'
-import './assets/fonts/Streamster.woff;'
-import './assets/fonts/Streamster.woff2'
+import './assets/fonts/LarabiefontRg-Regular.eot';
+import './assets/fonts/LarabiefontRg-Regular.svg';
+import './assets/fonts/LarabiefontRg-Regular.ttf';
+import './assets/fonts/LarabiefontRg-Regular.woff';
+import './assets/fonts/LarabiefontRg-Regular.woff2';
+import './assets/fonts/Streamster.eot';
+import './assets/fonts/Streamster.svg';
+import './assets/fonts/Streamster.ttf';
+import './assets/fonts/Streamster.woff';
+import './assets/fonts/Streamster.woff2';
 
 render(
   <App />,
