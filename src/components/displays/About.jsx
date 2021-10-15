e7d0ae3cfd92a1d4d57cb3dd683ad03170fc2f1e
Elise Muellerleile <eliamue@live.com>
2021-10-14 20:35:53 -0500

    bios fixed

--- src/components/displays/About.jsx
+++ src/components/displays/About.jsx
@@ -39,11 +39,7 @@ export default class About extends Component {
             <p
               className={styles.bri_bio}
             >
-              A full-stack software engineer with a background in hospitality. I
-              champion human-centered designs built on strong, efficient
-              foundations using collaborative problem-solving. I enjoy learning
-              new technologies and working on diverse teams while implementing a
-              strong sense of community.
+              A full-stack software engineer with a background in hospitality. I champion human-centered designs built on strong, efficient foundations using collaborative problem-solving. I enjoy learning new technologies and working on diverse teams while implementing a strong sense of community.
             </p>
           </section>
 
@@ -69,14 +65,7 @@ export default class About extends Component {
             <p
               className={styles.elise_bio}
             >
-              A full-stack software engineer from St Paul, Minnesota with a
-              background in psychology, CCBHC mental health, and board game
-              publishing. My passion is in combining this background with my
-              newly honed skills in software development to work with a team on
-              creating and maintaining inclusive apps that accommodate the needs
-              of marginalized communities. Always preferring to keep busy, I
-              fill my non-coding time with as much dance, D&D, studying French &
-              Korean, crafting, and board gaming as I can get!
+              A full-stack software engineer from St Paul, Minnesota with a background in psychology, CCBHC mental health, and board game publishing. My passion is in combining this background with my newly honed skills in software development to work with a team on creating and maintaining inclusive apps that accommodate the needs of marginalized communities. Always preferring to keep busy, I fill my non-coding time with as much dance, D&D, studying French & Korean, crafting, and board gaming as I can get!
             </p>
           </section>
           {/* Sofia*/}
@@ -105,12 +94,8 @@ export default class About extends Component {
             <p
               className={styles.sofia_bio}
             >
-              A full-stack software engineer based in the San Francisco Bay Area. I love
-              to code because it&apos;s an exciting and challenging field where
-              I can see myself being an asset to my community and where I know I
-              will feel fulfilled. When I&apos;m not coding, you can find me on
-              Steam, learning to be a better photographer, or reading novels. My
-              favorite languages are Korean and Spanish!
+              A full-stack software engineer based in the San Francisco Bay Area. I love to code because it&apos;s an exciting and challenging field where I can see myself being an asset to my community and where I know I
+              will feel fulfilled. When I&apos;m not coding, you can find me on Steam, learning to be a better photographer, or reading novels. My favorite languages are Korean and Spanish!
             </p>
           </section>
         </div>
