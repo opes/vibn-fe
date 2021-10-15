bd9c68f4694e0a9937406f27284cd15ced247fcc
Elise Muellerleile <eliamue@live.com>
2021-10-14 22:49:19 -0500

    loggedin skips pogin page and directs straight to user profile

--- src/components/displays/LoggedIn.jsx
+++ src/components/displays/LoggedIn.jsx
@@ -1,45 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import useUsers from '../../hooks/useUsers';
 import PropTypes from 'prop-types';
 import styles from '../../styles/loggedin.css';
-import linebreak from '../../assets/linebreak.png';
-import vibn from '../../assets/vibn.png';
+// import linebreak from '../../assets/linebreak.png';
+// import vibn from '../../assets/vibn.png';
+
+const spinner =
+  'https://64.media.tumblr.com/2e207597333f8528f39870b5b72e800c/tumblr_n8l3gq3Ygs1qza1qzo1_500.gifv';
 
 export default function LoggedIn({ match }) {
   const history = useHistory();
   localStorage.setItem('REFRESH_TOKEN', match.params.refresh_token);
   localStorage.setItem('ACCESS_TOKEN', match.params.access_token);
   localStorage.setItem('CURRENT_USER_ID', match.params.id);
-
   const { userObject } = useUsers();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // history.push(`https://vibn.netlify.app/user/${userObject.id}/dash`);
-    history.push(`/user/${userObject.id}/dash`);
-  };
-
-  const handleSignout = (event) => {
-    event.preventDefault();
-    localStorage.setItem('REFRESH_TOKEN', '');
-    localStorage.setItem('ACCESS_TOKEN', '');
-    localStorage.setItem('CURRENT_USER_ID', '');
-    window.localStorage.clear();
-    window.location.href = '/';
-  };
+  useEffect(() => {
+    setTimeout(() => {
+      history.push((`/user/${userObject.id}/dash`), 9000);
+      return () => spinner;
+    }, []);
+  });
 
   return (
     <div className={styles.login_body}>
-      <img className={styles.vibn_logo} src={vibn} alt="vibn logo" />
-      <p className={styles.vibn_desc}>Find your music match</p>
-      <img className={styles.linebreak} src={linebreak} alt="linebreak" />
-      <form onSubmit={handleSubmit}>
-        <button className={styles.login_btn}>Sign in</button>
-      </form>
-      <form onSubmit={handleSignout}>
-        <button className={styles.logout_btn}>Sign Out</button>
-      </form>
+      <img className={styles.spinner} src={spinner} alt="vibn logo" />
     </div>
   );
 }
