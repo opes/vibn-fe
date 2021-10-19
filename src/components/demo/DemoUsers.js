62d73a94290c85c63e6efa015f1ce282f61eb003
Brianna Bias <brianna.bias@gmail.com>
2021-10-18 18:28:30 -0700

    added id to demo users

--- src/components/demo/DemoUsers.js
+++ src/components/demo/DemoUsers.js
@@ -1,19 +1,22 @@
 const demoUsers = [
-  {
+    {
+      id: '89173584726789',
     displayName: 'Gary Busey',
     email: 'gary@busey.co',
     profileURL: 'http://spotify.com',
     image:
       'https://images.mubicdn.net/images/cast_member/2321/cache-463-1602494874/image-w856.jpg',
   },
-  {
+    {
+      id: '23687489823',
     displayName: 'Sbeve II',
     email: 'sbeve@busey.co',
     profileURL: 'http://spotify.com',
     image:
       'https://upload.wikimedia.org/wikipedia/en/8/8b/ST3_Steve_Harrington_portrait.jpg',
   },
-  {
+    {
+      id: '02934562038',
     displayName: 'Steve Buscemi',
     email: 'steve@busey.co',
     profileURL: 'http://spotify.com',
