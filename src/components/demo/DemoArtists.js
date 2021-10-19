76673ef57a6cb28864edc798d36c147915e17b28
Elise Muellerleile <79237986+eliamue@users.noreply.github.com>
2021-10-19 18:37:36 -0500

    Ugh (#45)
    * demo folder and pages added
    
    * demoservices
    
    * demoservices functions added
    
    * demo users and demo artist datat added
    
    * 20 demo artists
    
    * demo stuff
    
    * background of conversations fixed
    
    * demo user artists rendering

--- src/components/demo/DemoArtists.js
+++ src/components/demo/DemoArtists.js
@@ -1,43 +1,44 @@
 const demoArtists = [
+
   {
-    id: '2SVw939fwuqSobLjF8u78b',
-    artistName: 'Gogol Bordello',
+    id: '6ueGR6SWhUJfvEhqkvMsVs',
+    artistName: 'Janelle Monáe',
     artistImage:
-      'https://i.scdn.co/image/ab6761610000e5eb7c146afccb1159cb0a79acc3',
-    artistUrl: 'https://open.spotify.com/artist/2SVw939fwuqSobLjF8u78b',
-    artistGenre: ['gypsy', 'hardcore', 'screamo'],
+      'https://i.scdn.co/image/ab676161000051743ea2c03525939b482f8d3bfd',
+    artistUrl: 'https://open.spotify.com/artist/6ueGR6SWhUJfvEhqkvMsVs',
+    artistGenre: ['afrofuturism', 'alternative r&b'],
   },
   {
-    id: '4iJLPqClelZOBCBifm8Fzv',
-    artistName: 'Pierce The Veil',
+    id: '6zvul52xwTWzilBZl6BUbT',
+    artistName: 'Pixies',
     artistImage:
-      'https://i.scdn.co/image/ab6761610000e5eb58c744a03131b95b9e846ff5',
-    artistUrl: 'https://open.spotify.com/artist/4iJLPqClelZOBCBifm8Fzv',
-    artistGenre: ['post-hardcore', 'hardcore', 'screamo'],
+      'https://i.scdn.co/image/ab67616100005174c93891745e7444b6d3edd341',
+    artistUrl: 'https://open.spotify.com/artist/6zvul52xwTWzilBZl6BUbT',
+    artistGenre: ['alternative rock', 'boston rock'],
   },
   {
-    id: '4LLpKhyESsyAXpc4laK94U',
-    artistName: 'Mac Miller',
+    id: '7jVv8c5Fj3E9VhNjxT4snq',
+    artistName: 'Lil Nas X',
     artistImage:
-      'https://i.scdn.co/image/ab6761610000e5ebed3b89aa602145fde71a163a',
-    artistUrl: 'https://spotify.com/artist/4LLpKhyESsyAXpc4laK94U',
-    artistGenre: ['hip hop', 'rap'],
+      'https://i.scdn.co/image/ab67616100005174ab6bd6e450cbc7629a9a2381',
+    artistUrl: 'https://open.spotify.com/artist/7jVv8c5Fj3E9VhNjxT4snq',
+    artistGenre: ['lgbtq+ hip hop', 'pop'],
   },
   {
-    id: '7GN9PivdemQRKjDt4z5Zv8',
-    artistName: 'The Internet',
+    id: '73sIBHcqh3Z3NyqHKZ7FOL',
+    artistName: 'Childish Gambino',
     artistImage:
-      'https://i.scdn.co/image/ab6761610000e5ebc32c5dcda378eb561f81b1f9',
-    artistUrl: 'https://open.spotify.com/artist/7GN9PivdemQRKjDt4z5Zv8',
-    artistGenre: ['hip hop', 'rap'],
+      'https://i.scdn.co/image/ab676161000051743ef779aa0d271adb2b6a3ded',
+    artistUrl: 'https://open.spotify.com/artist/73sIBHcqh3Z3NyqHKZ7FOL',
+    artistGenre: ['atl hip hop', 'hip hop'],
   },
   {
-    id: '2EMAnMvWE2eb56ToJVfCWs',
-    artistName: 'Bryson Tiller',
+    id: '7bmYpVgQub656uNTu6qGNQ',
+    artistName: 'Hwa Sa',
     artistImage:
-      'https://i.scdn.co/image/ab6761610000e5eb078fdd734b7f0aa782328428',
-    artistUrl: 'https://open.spotify.com/artist/2EMAnMvWE2eb56ToJVfCWs',
-    artistGenre: ['hip hop', 'rap'],
+      'https://i.scdn.co/image/ab6761610000517410a7ade2827db798b9347c93',
+    artistUrl: 'https://open.spotify.com/artist/7bmYpVgQub656uNTu6qGNQ',
+    artistGenre: ['kpop', 'korean mask singer'],
   },
   {
     id: '1z4g3DjTBBZKhvAroFlhOM',
