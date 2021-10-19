6a64399999e55b623d7264d473b5d9ea522e9c79
Brianna Bias <brianna.bias@gmail.com>
2021-10-18 18:26:23 -0700

    demo users

---
+++ src/components/demo/DemoUsers.js
@@ -0,0 +1,25 @@
+const demoUsers = [
+  {
+    displayName: 'Gary Busey',
+    email: 'gary@busey.co',
+    profileURL: 'http://spotify.com',
+    image:
+      'https://images.mubicdn.net/images/cast_member/2321/cache-463-1602494874/image-w856.jpg',
+  },
+  {
+    displayName: 'Sbeve II',
+    email: 'sbeve@busey.co',
+    profileURL: 'http://spotify.com',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/8/8b/ST3_Steve_Harrington_portrait.jpg',
+  },
+  {
+    displayName: 'Steve Buscemi',
+    email: 'steve@busey.co',
+    profileURL: 'http://spotify.com',
+    profileImage:
+      'https://cdn2.newsok.biz/cache/r960-82984c26d6d75dbc514b6fe25a0ccc29.jpg',
+  },
+];
+
+export default demoUsers;
