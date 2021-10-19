325d398471ec1309936567d2dd159660628509e1
Elise Muellerleile <eliamue@live.com>
2021-10-18 20:42:16 -0500

    demoservices functions added

--- src/components/demo/DemoServices.js
+++ src/components/demo/DemoServices.js
@@ -1,25 +1,5 @@
-const URL = './DemoArtists.js';
-
-export const fetchAllDemoUsers = async () => {
-  const res = await fetch(`${URL}users`);
-  const userList = await res.json();
-
-  return userList;
-};
-
-export const fetchDemoUserById = async (id) => {
-  const res = await fetch(`${URL}users/${id}`);
-  const user = await res.json();
-
-  return user;
-};
-
-export const fetchDemoUserArtists = async (id) => {
-  const res = await fetch(`${URL}artists/${id}`);
-  const artistsArray = await res.json();
-
-  return artistsArray;
-};
+const artistsURL = './DemoArtists.js';
+const usersURL = './DemoUsers.js';
 
 export function findById(demoUserArray, demoId) {
   const numberId = Number(demoId);
@@ -29,4 +9,37 @@ export function findById(demoUserArray, demoId) {
 
   return null;
 }
+
+export const fetchAllDemoUsers = async () => {
+  const res = await fetch(usersURL);
+  const users = await res.json();
+
+  return users;
+};
+
+export const fetchDemoUserById = async (id) => {
+  const userList = fetchAllDemoUsers();
+  const res = findById(userList, id);
+  const user = await res.json();
+
+  return user;
+};
+
+export const fetchDemoArtists = async () => {
+  const res = await fetch(artistsURL);
+  const artists = await res.json();
+
+  return artists;
+};
+
+export const fetchDemoUserArtistsById = async (id) => {
+  const artistList = fetchDemoArtists();
+  const res = findById(artistList, id);
+
+  const artistsArray = await res.json();
+
+  return artistsArray;
+};
+
+
 
--- src/components/demo/DemoUsers.js
+++ src/components/demo/DemoUsers.js
@@ -1,25 +1,28 @@
 const demoUsers = [
-    {
-      id: '89173584726789',
+  {
+    id: '89173584726789',
     displayName: 'Gary Busey',
     email: 'gary@busey.co',
-    profileURL: 'http://spotify.com',
+    profileURL:
+      'https://open.spotify.com/playlist/0mfhWrW3IKTUk6nHvO3Z7n?si=1c29160454b748a0',
     image:
       'https://images.mubicdn.net/images/cast_member/2321/cache-463-1602494874/image-w856.jpg',
   },
-    {
-      id: '23687489823',
+  {
+    id: '23687489823',
     displayName: 'Sbeve II',
     email: 'sbeve@busey.co',
-    profileURL: 'http://spotify.com',
+    profileURL:
+      'https://open.spotify.com/playlist/10PtRgVMX2LKPpjSn6kMio?si=5ceab0e4767f461b',
     image:
       'https://upload.wikimedia.org/wikipedia/en/8/8b/ST3_Steve_Harrington_portrait.jpg',
   },
-    {
-      id: '02934562038',
+  {
+    id: '02934562038',
     displayName: 'Steve Buscemi',
     email: 'steve@busey.co',
-    profileURL: 'http://spotify.com',
+    profileURL:
+      'https://open.spotify.com/show/1w6X7As4rauic2UQEBoJ3O?si=Bc68QGPzRbyngn5krBOSdg',
     profileImage:
       'https://cdn2.newsok.biz/cache/r960-82984c26d6d75dbc514b6fe25a0ccc29.jpg',
   },
